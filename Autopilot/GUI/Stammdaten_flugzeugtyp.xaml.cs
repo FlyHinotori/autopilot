cc53559e50e7581c8fda77517fcd544fe8dd188d
Torsten Noack <torsten@schlummweg.de>
2015-06-17 17:42:36 +0200

    Flugzeugtypen fertig!

--- Autopilot/GUI/Stammdaten_flugzeugtyp.xaml.cs
+++ Autopilot/GUI/Stammdaten_flugzeugtyp.xaml.cs
@@ -33,10 +33,6 @@ namespace Autopilot.GUI
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            //System.Windows.Data.CollectionViewSource autopilotEntitiesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("autopilotEntitiesViewSource")));
-            //content.flugzeugtyp.Load();
-            //autopilotEntitiesViewSource.Source = content.flugzeugtyp.Local;
-            
             flugzeugtypDataGrid.ItemsSource = GetList();            
         }
 
@@ -58,6 +54,15 @@ namespace Autopilot.GUI
                     flugzeugtyp.ftyp_anz_ccrew = flzt.ftyp_anz_ccrew;
                     flugzeugtyp.ftyp_anz_fcrew = flzt.ftyp_anz_fcrew;
                     flugzeugtyp.ftyp_anz_pass = flzt.ftyp_anz_pass;
+                    flugzeugtyp.ftyp_anz_triebwerke = flzt.ftyp_anz_triebwerke;
+                    flugzeugtyp.ftyp_bez = flzt.ftyp_bez;
+                    flugzeugtyp.ftyp_fkosten_pa = flzt.ftyp_fkosten_pa;
+                    flugzeugtyp.ftyp_id = flzt.ftyp_id;
+                    flugzeugtyp.ftyp_reichweite_km = flzt.ftyp_reichweite_km;
+                    flugzeugtyp.ftyp_speed = flzt.ftyp_speed;
+                    flugzeugtyp.ftyp_vkosten_ph = flzt.ftyp_vkosten_ph;
+                    flugzeugtyp.her_id = flzt.her_id;
+                    flugzeugtyp.twa_id = flzt.twa_id;
                     content.flugzeugtyp.Add(flugzeugtyp);
                     content.SaveChanges();
                     flugzeugtypDataGrid.ItemsSource = GetList();
@@ -68,6 +73,41 @@ namespace Autopilot.GUI
             }
 
             content.SaveChanges();
-        }        
+        }
+
+        private void flugzeugtypDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete && !isBeingEdited)
+            {
+                var grid = (DataGrid)sender;
+                if (grid.SelectedItems.Count > 0)
+                {
+                    var Res = MessageBox.Show("Möchten Sie wirklich " + grid.SelectedItems.Count + " Flugzeugtyp(en) löschen?", "Löschen", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                    if (Res == MessageBoxResult.Yes)
+                    {
+                        foreach (var row in grid.SelectedItems)
+                        {
+                            flugzeugtyp flugzeugtyp = row as flugzeugtyp;
+                            content.flugzeugtyp.Remove(flugzeugtyp);
+                        }
+                        content.SaveChanges();
+                        MessageBox.Show(grid.SelectedItems.Count + " Flugzeugtyp(en) wurden gelöscht!");
+                    }
+                    else
+                        flugzeugtypDataGrid.ItemsSource = GetList();
+                }
+            }
+        }
+
+        private void flugzeugtypDataGrid_AddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            isInsertMode = true;
+        }
+
+        private void flugzeugtypDataGrid_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            isBeingEdited = true;
+        }
+
     }
 }
