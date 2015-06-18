9133a055cd10842f0684ea01d0615c54c0de7454
Torsten Noack <torsten@schlummweg.de>
2015-06-18 13:25:47 +0200

    Positionen zugefügt

--- Autopilot/GUI/Einstellungen.xaml.cs
+++ Autopilot/GUI/Einstellungen.xaml.cs
@@ -71,5 +71,10 @@ namespace Autopilot.GUI
         {
             Inhaltsanzeige.Navigate(new Uri("GUI/Stammdaten_mahnstufe.xaml", UriKind.Relative));
         }
+
+        private void Positionen_anzeigen(object sender, RoutedEventArgs e)
+        {
+            Inhaltsanzeige.Navigate(new Uri("GUI/Stammdaten_position.xaml", UriKind.Relative));
+        }
     }
 }
