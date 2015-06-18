5f078b7bb5ed74ffe06942bee245979f174b8096
Torsten Noack <torsten@schlummweg.de>
2015-06-18 13:42:23 +0200

    Status zugefügt

--- Autopilot/GUI/Einstellungen.xaml.cs
+++ Autopilot/GUI/Einstellungen.xaml.cs
@@ -81,5 +81,10 @@ namespace Autopilot.GUI
         {
             Inhaltsanzeige.Navigate(new Uri("GUI/Stammdaten_positionsart.xaml", UriKind.Relative));
         }
+
+        private void Status_anzeigen(object sender, RoutedEventArgs e)
+        {
+            Inhaltsanzeige.Navigate(new Uri("GUI/Stammdaten_status.xaml", UriKind.Relative));
+        }
     }
 }
