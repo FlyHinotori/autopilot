e4918ec31dd35fbaf5a376df998a48ce27860508
Hannes <hannes.marien@gmail.com>
2015-07-03 20:56:52 +0200

    Neue Property für den Auftragsstatus

--- Autopilot/Models/AuftragModel.cs
+++ Autopilot/Models/AuftragModel.cs
@@ -7,15 +7,18 @@ using System.ComponentModel;
 
 namespace Autopilot.Models
 {
+    enum AuftragStatus { Aufnahme, Angebot, Vertrag, Durchführung, Beendet, Storno };
     public class AuftragModel : INotifyPropertyChanged
     {
         public AuftragModel()
         {
             FKunde = new KundeModel();
+            FStatus = AuftragStatus.Aufnahme;
         }
 
         //members
         KundeModel FKunde;
+        AuftragStatus FStatus;
 
         #region properties
         public KundeModel Kunde
@@ -25,6 +28,15 @@ namespace Autopilot.Models
             {
                 FKunde = value;
                 NotifyPropertyChanged("Kunde");
+            }
+        }
+        public AuftragStatus Status
+        {
+            get { return FStatus; }
+            set
+            {
+                FStatus = value;
+                NotifyPropertyChanged("Status");
             }
         }
         #endregion
