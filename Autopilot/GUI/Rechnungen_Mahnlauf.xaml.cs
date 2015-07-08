9bc5204c80d6ab51041a1e8b5951203bb73bea0d
Torsten Noack <torsten@schlummweg.de>
2015-07-08 23:09:25 +0200

    Mahnlauf - Mahnstufe 2

--- Autopilot/GUI/Rechnungen_Mahnlauf.xaml.cs
+++ Autopilot/GUI/Rechnungen_Mahnlauf.xaml.cs
@@ -27,6 +27,7 @@ namespace Autopilot.GUI
         string DBconnStrg = Properties.Settings.Default.AutopilotConnectionString;
         public string SQLCmd_txt;
         DataTable datatableSTAID33 = new DataTable("STAID33");
+        DataTable datatableSTAID37 = new DataTable("STAID37");
 
         public Rechnungen_Mahnlauf()
         {
@@ -127,14 +128,14 @@ namespace Autopilot.GUI
         private void bt_ManhlaufDurchf_Click(object sender, RoutedEventArgs e)
         {
             string SQLcmd00 = "SELECT auf_id FROM auftrag, kunde WHERE auftrag.knd_id = kunde.knd_id AND kng_id != 1 AND auf_faellig_am < CONVERT(date,\'" + DateTime.Now + "\',103) AND (select (case when sum(buc_haben) is null then 0 else sum(buc_haben) end) - (case when sum(buc_soll) is null then 0 else sum(buc_soll) end) from buchung where buchung.auf_id = auftrag.auf_id) ";
+            SqlConnection conn = new SqlConnection(DBconnStrg);
             
-            //Rechnungen mit Saldo größer gleich NULL, fällig und STA_ID = 32 --> STA_ID = 33
+            //Rechnungen mit Saldo größer gleich NULL, fällig und STA_ID = 32/34/35/36/37 --> STA_ID = 33
             datatableSTAID33.Clear();
 
-            SqlConnection conn = new SqlConnection(DBconnStrg);
             conn.Open();
 
-            string SQLcmd33 = SQLcmd00 + " >= 0  AND auftrag.sta_id = 32 ";
+            string SQLcmd33 = SQLcmd00 + " >= 0  AND (auftrag.sta_id = 32 OR auftrag.sta_id = 34 OR auftrag.sta_id = 35 OR auftrag.sta_id = 36 OR auftrag.sta_id = 37)";
             SqlDataAdapter adapter33 = new SqlDataAdapter(SQLcmd33, conn);
             adapter33.Fill(datatableSTAID33);
 
@@ -143,6 +144,43 @@ namespace Autopilot.GUI
             foreach (DataRow row in datatableSTAID33.Rows)
             {
                 SQLCmd_txt = "UPDATE auftrag SET sta_id = 33 WHERE auf_id = " + row[0].ToString();
+                SQLCmd();
+            }
+
+            //Rechnungen mit Saldo kleiner NULL, fällig und STA_ID = 36 --> STA_ID = 37
+            datatableSTAID37.Clear();
+
+            conn.Open();
+
+            string SQLcmd37 = SQLcmd00 + " < 0  AND auftrag.sta_id = 36 ";
+            SqlDataAdapter adapter37 = new SqlDataAdapter(SQLcmd37, conn);
+            adapter37.Fill(datatableSTAID37);
+
+            conn.Close();
+
+            foreach (DataRow row in datatableSTAID37.Rows)
+            {
+                SQLCmd_txt = "UPDATE auftrag SET sta_id = 37, mst_id = 2 WHERE auf_id = " + row[0].ToString();
+                SQLCmd();
+                
+                //Mahngebühr berechnen
+                decimal mahngebuehr = 0;
+                
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT (auf_preis + auf_zusatzkosten) * mst_zuschlag FROM auftrag, mahnstufe WHERE auftrag.mst_id = mahnstufe.mst_id AND auf_id = " + row[0].ToString();
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    mahngebuehr = Convert.ToDecimal(dr.GetValue(0));
+                }
+                conn.Close();
+                
+                SQLCmd_txt = "INSERT INTO buchung (auf_id,buc_datum,buc_soll,buc_text) VALUES (" + row[0].ToString() + ",CONVERT(date,\'" + DateTime.Now + "\',103)," + mahngebuehr.ToString().Replace(",",".") + ",\'Mahngebühr Stufe 2\')";
                 SQLCmd();
             }
 
