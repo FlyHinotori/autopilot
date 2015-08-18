0aa7525f1ae2d37e51bac7da962c2e9f320dff4b
Hannes <hannes.marien@gmail.com>
2015-08-18 20:17:31 +0200

    Fertig
    Distanz wird nun beim Speichern des Auftrags geprüft und es wird eine
    Fehlermeldung angezeigt, wenn das Flugzeug nicht weit genug fliegt.

--- Autopilot/Models/AuftragModel.cs
+++ Autopilot/Models/AuftragModel.cs
@@ -319,8 +319,38 @@ namespace Autopilot.Models
         }
         #endregion
 
+        private void CheckDoability()
+        {
+            //Check the distance
+            DistanceChecker DC = new DistanceChecker();
+            switch (FArtID)
+            {
+                //Einzelflug
+                case 1:
+                    DC.AddFlughafen(FContent.flughafen.Where(fh => fh.flh_id == FStartFlughafenID).FirstOrDefault());
+                    DC.AddFlughafen(FContent.flughafen.Where(fh => fh.flh_id == FZielFlughafenID).FirstOrDefault());
+                    break;                      
+                //Einzelflug + Zwischenhalt
+                case 2:
+                    DC.AddFlughafen(FContent.flughafen.Where(fh => fh.flh_id == FStartFlughafenID).FirstOrDefault());
+                    foreach (Autopilot.flughafen Flughafen in FZwischenHalte)
+                        DC.AddFlughafen(Flughafen);
+                    DC.AddFlughafen(FContent.flughafen.Where(fh => fh.flh_id == FZielFlughafenID).FirstOrDefault());
+                    break;
+                //Zeitcharter - Distanz egal
+                case 3:
+                default:
+                    return;
+            }
+            if (DC.CanFlugzeugDoTheFlight(FContent.flugzeugtyp.Where(ft => ft.ftyp_id == FFlugzeugTypID).FirstOrDefault()))
+                return;
+            else
+                throw new AuftragDatenFehlerhaftException("Dieser Flugzeugtyp ist nicht für diese Distanz geeignet.");
+        }
+
         public void Save()
         {
+            CheckDoability();
             FKunde.Save();
             SaveAuftrag();
             SaveTermin();
