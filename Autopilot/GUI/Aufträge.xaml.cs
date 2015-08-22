84ad530e0be810f6f537b4a7a6679500f5fbee02
Hannes <hannes.marien@gmail.com>
2015-08-22 17:26:59 +0200

    Aufträge nach Status sortiert
    Ein früher Status (z.B. Angebot) steht weiter oben als ein später (z.B.
    Vertrag)

--- Autopilot/GUI/Aufträge.xaml.cs
+++ Autopilot/GUI/Aufträge.xaml.cs
@@ -38,7 +38,7 @@ namespace Autopilot.GUI
             //Aufträge laden
             string SQLcmd = "SELECT a.auf_id, a.knd_id, anr.anr_bez, k.knd_vorname, k.knd_name, t.ter_beginn, t.ter_ende, s.sta_bez"
                 + " FROM auftrag a LEFT JOIN kunde k ON (k.knd_id = a.knd_id) LEFT JOIN anrede anr ON (anr.anr_id = k.anr_id) LEFT JOIN status s ON (s.sta_id = a.sta_id)"
-                + " LEFT JOIN termin_auftrag ta ON (ta.auf_id = a.auf_id) LEFT JOIN termin t ON (t.ter_id = ta.ter_id)";
+                + " LEFT JOIN termin_auftrag ta ON (ta.auf_id = a.auf_id) LEFT JOIN termin t ON (t.ter_id = ta.ter_id) ORDER BY a.sta_id ASC";
             SqlCommand cmd = new SqlCommand(SQLcmd, conn);
             SqlDataAdapter adapter = new SqlDataAdapter(cmd);
             adapter.Fill(TableAuftraege);
