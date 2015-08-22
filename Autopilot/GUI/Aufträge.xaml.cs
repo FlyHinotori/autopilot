6ca08a0c4309d81d50412bf614020f21d19d2935
Hannes <hannes.marien@gmail.com>
2015-08-22 16:56:43 +0200

    Grid zur Darstellung der Aufträge

--- Autopilot/GUI/Aufträge.xaml.cs
+++ Autopilot/GUI/Aufträge.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Autopilot.GUI
 {
@@ -20,9 +22,28 @@ namespace Autopilot.GUI
     /// </summary>
     public partial class Aufträge : Page
     {
+        string DBconnStrg = Properties.Settings.Default.AutopilotConnectionString;
+        DataTable TableAuftraege = new DataTable("Auftraege");
+
         public Aufträge()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            TableAuftraege.Clear();
+            SqlConnection conn = new SqlConnection(DBconnStrg);
+
+            //Aufträge laden
+            string SQLcmd = "SELECT a.auf_id, a.knd_id, anr.anr_bez, k.knd_vorname, k.knd_name, t.ter_beginn, t.ter_ende, s.sta_bez"
+                + " FROM auftrag a LEFT JOIN kunde k ON (k.knd_id = a.knd_id) LEFT JOIN anrede anr ON (anr.anr_id = k.anr_id) LEFT JOIN status s ON (s.sta_id = a.sta_id)"
+                + " LEFT JOIN termin_auftrag ta ON (ta.auf_id = a.auf_id) LEFT JOIN termin t ON (t.ter_id = ta.ter_id)";
+            SqlCommand cmd = new SqlCommand(SQLcmd, conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(TableAuftraege);
+
+            GridAuftraege.ItemsSource = TableAuftraege.DefaultView;
         }
     }
 }
